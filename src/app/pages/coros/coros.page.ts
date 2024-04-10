b39a6e8c4456fef5241f5938782777411a19a3af
unknown <jesuslv2412@hotmail.com>
2024-04-09 23:33:00 -0600

    Reproductor Terminado

--- src/app/pages/coros/coros.page.ts
+++ src/app/pages/coros/coros.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonRange } from '@ionic/angular';
 import { Howl} from 'howler';
 
 export interface Track {
@@ -27,12 +28,18 @@ export class CorosPage implements OnInit {
     {
       name: 'Unbrekeable Resolve',
       path: '../assets/mp3/unbreakableresolve.mp3'
+    },
+    {
+      name: 'November Rain',
+      path: '../assets/mp3/Guns_N_Roses_-_November_Rain.mp3'
     }
   ];
 
   activeTrack: Track = null;
   player: Howl = null;
   isPlaying = false;
+  progress = 0;
+  @ViewChild('range', {static: false}) range: IonRange;
 
   constructor() { }
 
@@ -46,6 +53,7 @@ export class CorosPage implements OnInit {
         console.log('onplay');
         this.isPlaying = true;
         this.activeTrack = track;
+        this.updateProgess();
       },
       onend: () => {
         console.log('onend');
@@ -65,22 +73,35 @@ export class CorosPage implements OnInit {
   }
 
   next(){
-
+    let index = this.playlist.indexOf(this.activeTrack);
+    if( index != this.playlist.length - 1){
+      this.start(this.playlist[index + 1]);
+    } else {
+      this.start(this.playlist[0]);
+    }
   }
 
   prev(){
-
+    let index = this.playlist.indexOf(this.activeTrack);
+    if( index > 0){
+      this.start(this.playlist[index - 1])
+    } else {
+      this.start(this.playlist[this.playlist.length - 1])
+    }
   }
 
   seek(){
-
+    let newValue = +this.range.value;
+    let duration = this.player.duration();
+    this.player.seek(duration * (newValue / 100));
   }
-
   updateProgess(){
-
+    let seek = this.player.seek();
+    this.progress = (seek / this.player.duration()) * 100 || 0;
+    setTimeout(() => {
+      this.updateProgess();
+    }, 1000);
   }
-
-
 
   ngOnInit() {
   }
